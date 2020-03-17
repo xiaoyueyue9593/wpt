4ae8efed30ae21a7f0b1a2591f4375b1af4260f6
Rob Buis <rbuis@igalia.com>
2020-03-17 09:37:30 +0100

    Add test to verify that frame removal triggers revalidation cancellation
    Add test to verify that frame removal triggers revalidation
    cancellation.

--- fetch/stale-while-revalidate/resources/stale-script.py
+++ fetch/stale-while-revalidate/resources/stale-script.py
@@ -1,10 +1,13 @@
-import random, string, datetime
+import random, string, datetime, time
 
 def id_token():
    letters = string.ascii_lowercase
    return ''.join(random.choice(letters) for i in range(20))
 
 def main(request, response):
+    is_revalidation = request.headers.get("If-None-Match", None)
+    if is_revalidation is not None:
+      time.sleep(5);
     token = request.GET.first("token", None)
     is_query = request.GET.first("query", None) != None
     with request.server.stash.lock:
@@ -27,6 +30,7 @@ def main(request, response):
       unique_id = id_token()
       headers = [("Content-Type", "text/javascript"),
                  ("Cache-Control", "private, max-age=0, stale-while-revalidate=60"),
+                 ("ETag", '"swr"'),
                  ("Unique-Id", unique_id)]
       content = "report('{}')".format(unique_id)
       return 200, headers, content
